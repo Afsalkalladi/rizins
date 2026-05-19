75f49da8eabfe4eebff1aedd3957d36eadafa940
Afsalkalladi <afsalkalladi@gmail.com>
2026-05-19 15:17:49 +0530

    frankston uber eats and door dash

--- src/app/order-delivery/page.tsx
+++ src/app/order-delivery/page.tsx
@@ -27,6 +27,12 @@ const locations = [
     doordashUrl: "https://www.doordash.com/en-GB/store/rizin%E2%80%99s-smash-burgers-dandenong-24403281/17715024/?cursor=eyJzZWFyY2hfaXRlbV9jYXJvdXNlbF9jdXJzb3IiOnsicXVlcnkiOiJyaXppbnMgc21hc2ggYnVyZ2VycyIsIml0ZW1faWRzIjpbXSwic2VhcmNoX3Rlcm0iOiJyaXppbiBzbWFzaCBidXJnZXIiLCJ2ZXJ0aWNhbF9pZCI6LTk5OSwidmVydGljYWxfbmFtZSI6ImFsbCIsInF1ZXJ5X2ludGVudCI6IlNUT1JFX1JYIn0sInN0b3JlX3ByaW1hcnlfdmVydGljYWxfaWRzIjpbMSwxMTAwNDUsNCwxMDAzMzMsMTkyXX0=&pickup=false",
     uberEatsUrl: "https://www.ubereats.com/au/store/rizins-smash-burgers/mHLbgdAZQruoFtIhFWAdHg?diningMode=DELIVERY&mod=storeDeliveryTime&modctx=%257B%2522entryPoint%2522%253A%2522store-auto-surface%2522%252C%2522encodedStoreUuid%2522%253A%2522mHLbgdAZQruoFtIhFWAdHg%2522%257D&ps=1&surfaceName=",
   },
+  {
+    name: "Frankston",
+    id: "frankston",
+    doordashUrl: "https://www.doordash.com/en-AU/store/rizin’s-smash-burgers-frankston-42292193/104211546/?preview=1",
+    uberEatsUrl: "https://www.ubereats.com/store/rizins-smash-burgers-frankston/vzuG0_aQT_KakO4DmMl1rg?utm_source=menu-maker",
+  },
 ];
 
 function getUrl(platform: Platform, locId: string) {
